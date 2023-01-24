7a41055f1f63d33d68e6cc746defc09a89118008
kawan idrees <kawanedres211@gmail.com>
2023-01-24 17:23:15 +0300

    updated

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { mailOptions, transporter } from "../../config/Nodemailer";
+import { mailOptions, transporter } from "../../config/nodemailer";
 
 async function handler (req, res) {
   console.log(req.body);
@@ -21,14 +21,13 @@ async function handler (req, res) {
   res.status(200).json({ success:true })
 
     }catch(error){
-      // console.log(error.message);
       return   res.status(400).json({ message: error.message })
 
 
     }
 
   }
-  // res.status(400).json({ message: 'Bad Request' })
+ 
 }
 
 
